9f30f5e7d04d61894450b5c49291f092b8d28fed
arnargisla <arnargisla@gmail.com>
2019-02-12 22:14:16 +0100

    WIP Implement evaluation function
    I think there are some off by one errors, we need to decide on a
    convention for what pitting on lap 1 means.

--- src/optimizer.py
+++ src/optimizer.py
@@ -28,18 +28,44 @@ class Solution:
             )
         return s
 
+def _add_cumulative_to_laptimes(laptimes):
+    tyre_types = list(laptimes.keys())
+    for tyre_type in tyre_types:
+        cumulative_time_per_lap = []
+        cumulative_time = 0
+        for delta in laptimes[tyre_type]:
+            cumulative_time += delta.total_seconds()
+            cumulative_time_per_lap.append(cumulative_time)
+        laptimes[tyre_type + 'c'] = cumulative_time_per_lap
+    return laptimes
 
 def calculate_optimal_pit_stop_strategy(
     laptimes: Dict[str, timedelta], extra_info: Dict[str, Any]
 ) -> Tuple[Solution, int]:
+    _add_cumulative_to_laptimes(laptimes)
+    print("extra info")
+    print(extra_info)
+    print("laptimes")
+    print(laptimes)
     print("Generating possible solutions ...")
+
+    laps_in_race = extra_info["laps_in_race"]
+    average_pit_stop_time = extra_info["average_pit_time"]
+
+
+    #test solutions
+    test1 = Solution(pit_stop_laps= [5, 10, 20, 27], pit_tyre_choices= ['RH', 'RH', 'RS', 'RM'])
+    test1score = _evaluate_solution(test1, laps_in_race, average_pit_stop_time, laptimes)
+    return (test1, test1score)
+
     possible_solutions = _generate_possible_solutions(
-        extra_info["max_laps_per_tank"], extra_info["laps_in_race"]
+        extra_info["max_laps_per_tank"], laps_in_race
     )
+    possible_solutions = [next(possible_solutions)]
     # print(f"len(possible_solutions): {len([x for x in possible_solutions])}")
     print("Evaluating solutions ...")
     start_time = time()
-    scores = map(_evaluate_solution, possible_solutions)
+    scores = map(lambda s: _evaluate_solution(s, laps_in_race, laptimes, average_pit_stop_time), possible_solutions)
 
     pairs = zip(possible_solutions, scores)
     best_score = min(pairs, key=lambda p: p[1])
@@ -125,6 +151,35 @@ def _calculate_lap_diffs(pit_stop_laps):
     lap_diffs = [b - a for (a, b) in list(zip(v[::2], v[1::2]))]
     return lap_diffs
 
+def _get_time(laps_on_tyres, tyre_type, laptimes):
+    print(tyre_type+'c', laptimes[tyre_type + 'c'])
+    print(laptimes[tyre_type + 'c'][laps_on_tyres])
+    time = laptimes[tyre_type + 'c'][laps_on_tyres]
+    print(f"{laps_on_tyres} laps on {tyre_type} take {time} seconds")
+    return time
 
-def _evaluate_solution(solution: Solution) -> int:
-    return 1  # random score
+
+def _evaluate_solution(solution: Solution, laps_in_race: int, average_pit_stop_time: float, laptimes: Any) -> int:
+    solution.pit_stop_laps.append(laps_in_race+1)
+    solution.pit_tyre_choices.append("End")
+    time = 0
+    last_tyre_type = "RH"
+    last_lap = 0
+
+    print(f"evaluating solution: {solution}")
+    print(f"laps_in_race: {laps_in_race}")
+    print(f"average_pit_stop_time: {average_pit_stop_time}")
+
+    for lap, tyre_type in zip(solution.pit_stop_laps, solution.pit_tyre_choices):
+        laps_on_tyres = lap - last_lap
+        time += _get_time(laps_on_tyres, last_tyre_type, laptimes) + average_pit_stop_time
+        last_lap = lap
+        last_tyre_type = tyre_type
+        print("lap", lap, "tyre_type", tyre_type, f"time so far: {time}")
+
+    solution.pit_stop_laps.pop()
+    solution.pit_tyre_choices.pop()
+
+    print(f"total time for solution: {time} seconds")
+
+    return time
