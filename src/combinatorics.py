49715fd9b1214827d2b19e95fe44484123722796
hallgrimur1471 <hallgrimur1471@gmail.com>
2019-02-12 17:10:17 +0000

    WIP on generating solutions

--- src/combinatorics.py
+++ src/combinatorics.py
@@ -1,13 +1,52 @@
 #!/usr/bin/env python3.7
 
+# Standard library
+from typing import List, Any
+from functools import reduce
+import operator
 
-def gen_n_choose_k(n: List[int], k: int) -> List[List[int]]:
+
+def gen_k_combinations(n: List[Any], k: int) -> List[List[Any]]:
+    """
+    Also known as 'n choose k'
+    """
     if (k == 0) or (len(n) < k):
         return []
     if k == 1:
         return list(map(lambda x: [x], n))
     case1 = list(
-        map(lambda solution: solution + [n[-1]], _gen_n_choose_k(n[:-1], k - 1))
+        map(
+            lambda combination: combination + [n[-1]],
+            gen_k_combinations(n[:-1], k - 1),
+        )
     )
-    case2 = _gen_n_choose_k(n[1:], k)
+    case2 = gen_k_combinations(n[:-1], k)
     return case1 + case2
+
+
+def gen_k_multicombinations(n: List[Any], k: int) -> List[List[Any]]:
+    """
+    Also known as 'n choose k with repetition'
+    """
+    return _gen_k_multicombinations(n, k, [])
+
+
+def _gen_k_multicombinations(
+    n: List[Any], k: int, y: List[Any]
+) -> List[List[Any]]:
+    if k == 0:
+        return [y]
+    combinations = list(
+        map(lambda x: _gen_k_multicombinations(n, k - 1, y + [x]), n)
+    )
+    combinations = reduce(operator.add, combinations)
+    return combinations
+
+
+def test():
+    for solution in gen_k_multicombinations(list(range(1, 6)), 2):
+        print(solution)
+
+
+if __name__ == "__main__":
+    test()
--- src/optimizer.py
+++ src/optimizer.py
@@ -6,12 +6,13 @@ from typing import Generator, Tuple, Dict, List, Any
 from dataclasses import dataclass
 
 # This project
+import src.combinatorics as combinatorics
 
 
 @dataclass
 class Solution:
     pit_stop_laps: List[int]
-    pit_stop_tyres: List[str]
+    pit_tyre_choices: List[str]
 
 
 def calculate_optimal_pit_stop_strategy(
@@ -28,24 +29,39 @@ def calculate_optimal_pit_stop_strategy(
 def _generate_possible_solutions(
     max_laps_per_tank, laps_in_race
 ) -> Generator[Solution, None, None]:
-    possible_solutions = []
+    lap_list = list(range(1, laps_in_race + 1))
     max_pit_stops = 5
+
     for num_pit_stops in range(0, max_pit_stops + 1):
-        for solution in _generate_n_choose_k_solutions(
-            laps_in_race, num_pit_stops
-        ):
-            possible_solutions.append(solution)
-    print(f"len(possible_solutions): {len(possible_solutions)}")
-    possible_solutions = list(
-        filter(does_not_run_out_of_fuel, possible_solutions)
-    )
+        # _generate_solutions_with_num_pit_stops(laps_in_race, num_pit_stops)
+        pit_stop_laps_combinations = combinatorics.gen_k_combinations(
+            lap_list, num_pit_stops
+        )
+        for pit_stop_laps in pit_stop_laps_combinations:
+            pit_tyre_choices_combs = combinatorics.gen_k_multicombinations(
+                ["RH", "RM", "RS"], num_pit_stops
+            )
+            for pit_tyre_choices in pit_tyre_choices_combs:
+                yield Solution(
+                    pit_stop_laps=pit_stop_laps,
+                    pit_tyre_choices=pit_tyre_choices,
+                )
+
+    #    for solution in _generate_n_choose_k_solutions(
+    #        laps_in_race, num_pit_stops
+    #    ):
+    #        possible_solutions.append(solution)
+    # print(f"len(possible_solutions): {len(possible_solutions)}")
+    # possible_solutions = list(
+    #    filter(does_not_run_out_of_fuel, possible_solutions)
+    # )
     # pit_stop_solutions = _generate_n_choose_k_solutions(29, 5)
     # for solution in pit_stop_solutions:
     #    print(solution)
     # print(f"num_pit_stop_solutions: {len(pit_stop_solutions)}")
 
     for number in range(10):
-        yield Solution(pit_stop_laps=[], pit_stop_tyres=[])
+        yield Solution(pit_stop_laps=[], pit_tyre_choices=[])
 
 
 def does_not_run_out_of_fuel(pit_stop_solution):
@@ -54,19 +70,3 @@ def does_not_run_out_of_fuel(pit_stop_solution):
 
 def _evaluate_solution(solution: Solution) -> int:
     return 1  # random score
-
-
-def _generate_n_choose_k_solutions(n: int, k: int) -> List[List[int]]:
-    lap_solutions = combinatorics.gen_n_choose_k(list(range(1, n + 1)), k)
-    for lap_solution in lap_solutions:
-
-    solutions = list(
-        map(
-            lambda int_solution: Solution(
-                pit_stop_laps=int_solution, pit_stop_tyres=[]
-            ),
-            int_solutions,
-        )
-    )
-
-
