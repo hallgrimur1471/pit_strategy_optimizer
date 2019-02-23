bebd6f58bc0e48f5eb9389fa12b4e2bcffbe30c9
arnargisla <arnargisla@gmail.com>
2019-02-23 12:56:51 +0100

    Change the arguments, allowing for overrides
    hopefully fixed the bugs in evaluation

--- calculate_optimal_strategy.py
+++ calculate_optimal_strategy.py
@@ -11,11 +11,24 @@ from src.optimizer import Solution
 
 def main():
     arguments = parse_command_line_arguments()
-    (lap_times, extra_info) = spreadsheet.read_data(arguments.RACEDATA)
+    racedata = arguments.RACEDATA
+    if not racedata:
+        racedata = fetch_url_from_config()
+    (lap_times, extra_info) = spreadsheet.read_data(racedata)
     optimal_solution = optimizer.calculate_optimal_pit_stop_strategy(
-        lap_times, extra_info
+        lap_times, extra_info, average_pit_time=arguments.average_pit_time, laps_in_race_override=arguments.laps_in_race
     )
     print_solution(optimal_solution)
+
+def fetch_url_from_config():
+    try:
+        with open('config.cfg') as fd:
+            for line in fd:
+                return line.strip()
+    except IOError:
+        print("Error, you must specify the argument --racedata")
+        exit(1)
+
 
 
 def parse_command_line_arguments():
@@ -25,14 +38,21 @@ def parse_command_line_arguments():
         "printed to stdout as a human-readable text."
     )
     parser.add_argument(
-        "RACEDATA",
+        "--racedata-url",
+        dest="RACEDATA",
         help=(
-            "Link to a Google spreadhseet containing data about the race. "
-            "To see how the spreadhseet should be set up take a look at "
+            "Link to a Google spreadsheet containing data about the race. "
+            "To see how the spreadsheet should be set up take a look at "
             "this example: https://docs.google.com/spreadsheets/d/"
-            "1P3fT81u8-2S7sSsx5KlRKCovWY10ZBDjImO-9myxo9U/edit?usp=sharing"
+            "1P3fT81u8-2S7sSsx5KlRKCovWY10ZBDjImO-9myxo9U/edit?usp=sharing "
+            "if this argument is not provided the url can also be set in a "
+            "configuration file named config.cfg in the root of the project, "
+            "the first line in the config file should be the url to the "
+            "spreadsheet."
         ),
     )
+    parser.add_argument("--average-pit-time", dest="average_pit_time", type=float)
+    parser.add_argument("--laps-in-race", dest="laps_in_race", type=int)
     arguments = parser.parse_args()
     return arguments
 
--- src/optimizer.py
+++ src/optimizer.py
@@ -28,7 +28,7 @@ class Solution:
             else:
                 s = (
                     s
-                    + f" On lap nr. {self.pit_stop_laps[lap_index]} stop and use tyre type: {self.pit_tyre_choices[lap_index]}."
+                    + f" After lap nr. {self.pit_stop_laps[lap_index]} stop and use tyre type: {self.pit_tyre_choices[lap_index]}."
                 )
         return s
 
@@ -46,8 +46,9 @@ def _add_cumulative_to_laptimes(laptimes):
 
 
 def calculate_optimal_pit_stop_strategy(
-    laptimes: Dict[str, timedelta], extra_info: Dict[str, Any]
+        laptimes: Dict[str, List[timedelta]], extra_info: Dict[str, Any], average_pit_time:float=None, laps_in_race_override:int=None
 ) -> Tuple[Solution, int]:
+
 
     _add_cumulative_to_laptimes(laptimes)
 
@@ -60,8 +61,8 @@ def calculate_optimal_pit_stop_strategy(
         print(f"{tyre_type}: ")
         i = 0
         for delta in deltas:
-            s = ""
-            lap_nr = i+1
+            s = 0.0
+            lap_nr = i + 1
             if isinstance(delta, timedelta):
                 s = delta.total_seconds()
             else:
@@ -70,12 +71,19 @@ def calculate_optimal_pit_stop_strategy(
             i += 1
             if i % 5 == 0:
                 print()
-
+    
+    average_pit_stop_time = extra_info["average_pit_time"]
+    if(average_pit_time):
+        average_pit_stop_time = average_pit_time
+        print(f"Average pit time overwritten via command line argument {average_pit_time}")
+    
+    laps_in_race = extra_info["laps_in_race"]
+    if(laps_in_race_override):
+        laps_in_race = laps_in_race_override
+        print(f"Number of laps in race overwritten via command line argument {laps_in_race}")
 
     print("Generating possible solutions ...")
 
-    laps_in_race = extra_info["laps_in_race"]
-    average_pit_stop_time = extra_info["average_pit_time"]
     max_laps_per_tank = extra_info["max_laps_per_tank"]
 
     res = _does_not_run_out_of_fuel(
@@ -101,11 +109,16 @@ def calculate_optimal_pit_stop_strategy(
         possible_solutions,
     )
 
-    pairs = zip(possible_solutions, scores)
-    best_score = min(pairs, key=lambda p: p[1])
+    solution_score_pairs = zip(possible_solutions, scores)
+    best_solution_score_pair = min(solution_score_pairs, key=lambda p: p[1])
     evaluation_time = time() - start_time
     print(f"Evaluating solutions ... done in {evaluation_time} seconds")
-    return best_score
+
+    best_solution = best_solution_score_pair[0]
+    _evaluate_solution(
+        best_solution, laps_in_race, average_pit_stop_time, laptimes, verbose=True
+    )
+    return best_solution_score_pair
 
 
 def _generate_possible_solutions(
@@ -140,7 +153,7 @@ def _generate_possible_solutions(
             possible_solutions,
         )
     )
-    print(f"len(possible_solutions): {len(possible_solutions)}")
+    print(f"Number of generated possible solutions: {len(possible_solutions)}")
     number_of_possible_solutions_after_filtering = len(possible_solutions)
     number_of_filtered_solutions = (
         number_of_possible_solutions - number_of_possible_solutions_after_filtering
@@ -203,43 +216,53 @@ def _calculate_lap_diffs(pit_stop_laps, laps_in_race):
 
 
 def _get_time(laps_on_tyres, tyre_type, laptimes):
-    # print(f"laptimes: {laptimes}")
-    # print(f"tyre_type {tyre_type}")
-    # print(f"laps_on_tyres: {laps_on_tyres}")
-    # print("############")
-    # print(laptimes[tyre_type + 'c'][laps_on_tyres])
     if len(laptimes[tyre_type + "c"]) <= laps_on_tyres:
         print(f"illegal lap number {laps_on_tyres}")
-    time = laptimes[tyre_type + "c"][laps_on_tyres]
-    # print(f"{laps_on_tyres} laps on {tyre_type} take {time} seconds")
+    time = laptimes[tyre_type + "c"][laps_on_tyres-1]
     return time
 
 
 def _evaluate_solution(
-    solution: Solution, laps_in_race: int, average_pit_stop_time: float, laptimes: Any
-) -> int:
-    solution.pit_stop_laps.append(laps_in_race + 1)
-    solution.pit_tyre_choices.append("End")
+    solution: Solution,
+    laps_in_race: int,
+    average_pit_stop_time: float,
+    laptimes: Any,
+    verbose: bool = False,
+) -> float:
     time = 0
-    last_tyre_type = "RH"
-    last_lap = 0
 
-    # print(f"evaluating solution: {solution}")
-    # print(f"laps_in_race: {laps_in_race}")
-    # print(f"average_pit_stop_time: {average_pit_stop_time}")
+    if verbose:
+        print(f"Evaluating solution: {solution}")
 
-    for lap, tyre_type in zip(solution.pit_stop_laps, solution.pit_tyre_choices):
-        laps_on_tyres = lap - last_lap
-        time += (
-            _get_time(laps_on_tyres, last_tyre_type, laptimes) + average_pit_stop_time
+    lap_triples = list(
+        zip(
+            solution.pit_stop_laps,
+            solution.pit_tyre_choices,
+            range(len(solution.pit_stop_laps)),
         )
-        last_lap = lap
-        last_tyre_type = tyre_type
-        # print("lap", lap, "tyre_type", tyre_type, f"time so far: {time}")
+    )
 
-    solution.pit_stop_laps.pop()
-    solution.pit_tyre_choices.pop()
+    for lap, tyre_type, lap_index in lap_triples:
+        if lap_index + 1 == len(lap_triples):
+            laps_on_tyres = laps_in_race - lap
+        else:
+            next_lap = lap_triples[lap_index + 1][0]
+            laps_on_tyres = next_lap - lap
 
-    # print(f"total time for solution: {time} seconds")
+        time_to_drive = _get_time(laps_on_tyres, tyre_type, laptimes)
+        time += time_to_drive
+        if verbose:
+            print(
+                    f"after lap {lap} use tyre type: {tyre_type} for {laps_on_tyres} laps. This takes {time_to_drive}s, time so far: {time:.2f}s"
+            )
+
+    number_of_pit_stops = len(lap_triples) - 1
+    total_pit_stop_time = average_pit_stop_time * number_of_pit_stops
+    time += total_pit_stop_time
+    if verbose:
+        print(f"Adding {total_pit_stop_time}s for the {number_of_pit_stops} pit stops")
+
+    if verbose:
+        print(f"total time for solution: {time:.2f} seconds")
 
     return time
